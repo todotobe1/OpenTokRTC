4b8c0644f0b59934038ae1bf06711134203f1eec
SimonHYQ <cellvsu@qq.com>
2015-08-24 15:14:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ config.web = {};
 config.middleware = {};
 
 // OpenTok Credentials
-config.opentok.key = process.env.TB_KEY || 'Your opentok key';
-config.opentok.secret=  process.env.TB_SECRET || 'Your opentok secret';
+config.opentok.key = process.env.TB_KEY || '45320222';
+config.opentok.secret=  process.env.TB_SECRET || '0eeda647c4a920822878076acc300f1f61ebfdf9';
 
 // Database configuration. Do not change if you do not plan to use redis
 config.db.redis = false; // are you using redis?
